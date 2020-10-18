538ec9a6d3aabf229a1dafc71279e75617239d02
Joao Guilherme <tertuliano@sunnit.com.br>
2020-10-18 13:16:17 -0300

    feat: create route to add products and invoices

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,39 @@
 import express, { Router } from 'express'
+import mongoose, { mongo } from 'mongoose'
 
+import { Invoice } from './schemas/invoice'
+import { Product } from './schemas/product'
+
+import bodyParser from 'body-parser'
 const app = express()
 const router = Router()
+
+app.use(bodyParser.json())
+
+mongoose.connect(
+    'mongodb+srv://bolachadomar:1XhN7t3fi6daYyp3@parkingbike.gbdff.gcp.mongodb.net/invoice-issuer?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
 
 app.get('/', (req, res) => {
     return res.json({ message: 'Hello world' })
 })
 
 app.get('/product', (req, res) => {
-    const products = [{}]
+    const products = Product.find()
+    return res.json(products)
 })
 
-app.post('/invoice', (req, res) => {
-    const invoice = req.body
+app.post('/product', async (req, res) => {
+    const product = new Product(req.body)
+    const newProduct = await Product.create(product)
+    return res.json(newProduct)
+})
+
+app.post('/invoice', async (req, res) => {
+    const invoice = new Invoice(req.body)
+    const newInvoice = await Invoice.create(invoice)
+    return res.json(newInvoice)
 })
 
 app.listen(3000, () => {
