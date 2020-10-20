5e7660fd78f0a0fed4d0f7f792b250836d401fd8
Joao Guilherme <tertuliano@sunnit.com.br>
2020-10-20 20:11:38 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Router } from 'express'
+import express, { NextFunction, Request, Response, Router } from 'express'
 import mongoose from 'mongoose'
 
 import { Invoice } from './schemas/invoice'
@@ -6,9 +6,16 @@ import { Product } from './schemas/product'
 
 import bodyParser from 'body-parser'
 const app = express()
-const router = Router()
+
+const cors = (req: Request, res: Response, next: NextFunction): void => {
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-headers', '*')
+    next()
+}
 
 app.use(bodyParser.json())
+app.use(cors)
 
 mongoose.connect(
     'mongodb+srv://bolachadomar:1XhN7t3fi6daYyp3@parkingbike.gbdff.gcp.mongodb.net/invoice-issuer?retryWrites=true&w=majority',
@@ -19,9 +26,9 @@ app.get('/', (req, res) => {
     return res.json({ message: 'Hello world' })
 })
 
-app.get('/product', (req, res) => {
-    const products = Product.find()
-    return res.json(products)
+app.get('/product', async (req, res) => {
+    const products = await Product.find()
+    return products ? res.json(products) : res.json({})
 })
 
 app.post('/product', async (req, res) => {
